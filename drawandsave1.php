cb6a6db6e43f3cb8494410e87b8ca9e096556a3c
Jeff Sale <jsale@users.noreply.github.com>
2016-03-23 08:36:56 -0700

    updated file path for saved image.

--- drawandsave1.php
+++ drawandsave1.php
@@ -9,18 +9,15 @@ ini_set('post_max_size', '64M');
 
 $date_var = date('y_m_d_h_i_s');
 
-define('UPLOAD_DIR', 'images/screengrabs/');
-define('URL_DIR', 'http://localhost/fractal_drawing/images/screengrabs/');
+define('UPLOAD_DIR', './');
+define('URL_DIR', 'http://localhost/draw_and_save-master/');
 $img = $_POST['img'];
 $img = str_replace('data:image/png;base64,', '', $img);
 $img = str_replace(' ', '+', $img);
 $data = base64_decode($img);
 $filename = "saved_canvas.png";
 $savefile = UPLOAD_DIR . $filename;
-//$displayfile = URL_DIR . $filename;
 $success = file_put_contents($savefile, $data);
-//header("Location: http://localhost/fractal_drawing/images/screengrabs/$filename");
-
 
 ?>
 
